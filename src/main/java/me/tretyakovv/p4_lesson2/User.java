56b2720cb78e68033c1c8f83a913657bf391407a
TretyakovKonstantin <spb_unex@mail.ru>
2023-02-21 13:46:40 +0300

    Урок 2. Выполнено задание

---
+++ src/main/java/me/tretyakovv/p4_lesson2/User.java
@@ -0,0 +1,32 @@
+package me.tretyakovv.p4_lesson2;
+
+public class User {
+
+    private String login;
+
+    private String email;
+
+    public User() {
+    }
+
+    public User(String login, String email) {
+        this.login = login;
+        this.email = email;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
---
+++ src/test/java/me/tretyakovv/p4_lesson2/UserTest.java
@@ -0,0 +1,58 @@
+package me.tretyakovv.p4_lesson2;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.security.PublicKey;
+
+
+public class UserTest {
+
+    private String login;
+    private String email;
+
+    User user0;
+    User user1;
+
+    @BeforeEach
+    public void setUp() {
+
+        login = "test";
+        email = "test@mail.ru";
+
+        user0 = new User();
+        user1 = new User(login, email);
+    }
+
+    @Test
+    public void createUser() {
+        Assertions.assertEquals(login, user1.getLogin());
+        Assertions.assertEquals(email, user1.getEmail());
+    }
+
+    @Test
+    public void createUserEmpty() {
+        Assertions.assertNull(user0.getLogin());
+        Assertions.assertNull(user0.getEmail());
+    }
+
+    @Test
+    public void emailIsCorrect() {
+
+        String emailUser1 = user1.getEmail();
+
+        Assertions.assertNotNull(emailUser1);
+        if (emailUser1 != null)
+            Assertions.assertTrue(emailUser1.contains("@") && emailUser1.contains("."));
+    }
+
+    @Test
+    public void loginAndEmailEqual() {
+
+        String loginUser1 = user1.getLogin();
+        String emailUser1 = user1.getEmail();
+
+        Assertions.assertFalse(loginUser1.equals(emailUser1));
+    }
+}
